c46d2537972635d4996fbf02728c95da07d6d650
Rostislav Karchagin <rv_karchagin@me.com>
2022-09-17 15:38:25 +0300

    Проверка на число и вычисление треугодьн

--- MinboxTestLibrary/CalculateTriangle.cs
+++ MinboxTestLibrary/CalculateTriangle.cs
@@ -1,10 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MinboxTestLibrary;
 
 namespace MinboxTestLibrary
 {
-    public class CalculateTriangle
+    class CalculateTriangle
     {
-        public int SidOne = Console.ReadLine();
-    }
+        Check check = new Check();
 
+        public CalculateTriangle()
+        {
+        
+        }
+
+        private void GetSides(out int sideOne, out int sideTwo, out int sideThree)
+        {
+            Console.WriteLine("Введите значение стороны 1");
+
+            check.GetNumber(out sideOne);
+
+            Console.WriteLine("Введите значение стороны 2");
+
+            check.GetNumber(out sideTwo);
+
+            Console.WriteLine("Введите значение стороны 3");
+
+            check.GetNumber(out sideThree);
+        }
+
+        private void CalculateTriangleArea()
+        {
+            int sideOne;
+            int sideTwo;
+            int sideThree;
+
+            GetSides(out sideOne, out sideTwo, out sideThree);
+
+            int halfPerimetr = (sideOne + sideTwo + sideThree) / 2;
+            int area = Convert.ToInt32(Math.Sqrt(halfPerimetr * (halfPerimetr - sideOne) * (halfPerimetr - sideTwo) * (halfPerimetr - sideThree)));
+        }
+    }
 }
---
+++ MinboxTestLibrary/GetNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinboxTestLibrary
+{
+    class Check
+    {
+        public void GetNumber(out int number)
+        {
+            bool isNumberEntered = false;
+            string userInput = "";
+
+            while (isNumberEntered == false)
+            {
+                userInput = Console.ReadLine();
+                bool success = int.TryParse(userInput, out number);
+
+                if (success == true)
+                {
+                    isNumberEntered = true;
+                }
+                else
+                {
+                    Console.WriteLine("Введено не число. Повторите попытку");
+                }
+            }
+            number = int.Parse(userInput);
+            Console.WriteLine("Число " + number + " Записано");
+        }
+    }
+}
